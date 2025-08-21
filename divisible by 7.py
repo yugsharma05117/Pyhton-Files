3325158e171a7c0c60f792464bf84d0fd4029b38
Yug Sharma <syug200606@gmail.com>
2025-08-21 10:45:30 +0530

    Added a new file to check the last digit of a num- ber and modified the existing script to include an else condition for better clarity.

--- divisible by 7.py
+++ divisible by 7.py
@@ -3,3 +3,5 @@
 num=int(input("enter a number:"))
 if num%7==0:
     print("the number is divisible by 7")
+else:
+    print("the number is not divisible by 7")
---
+++ last_digit_4.py
@@ -0,0 +1,6 @@
+# Q2 WAP to check if the last digit is 4.
+num= int(input("Enter a number:"))
+if num%10 ==4:
+    print("Last digit is 4")
+else:
+    print("Last digit is not 4")
